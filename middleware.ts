9f2c8d629f763656a51eb088d5982c81d9ec7b4d
bamsi <sabkacade40@gmail.com>
2024-10-07 19:37:54 +0300

    updated

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,17 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  // console.log(pathname);
+  console.log(pathname);
   if (pathname === '/' && request.cookies.has('access_token'))
     return NextResponse.redirect(new URL('/dashboard', request.url));
 
   if (
-    pathname === '/dashboard/' ||
-    pathname === '/dashboard/product' ||
-    (pathname === '/dashboard/pos' && !request.cookies.has('access_token'))
-  )
+    pathname === '/dashboard' ||
+    pathname === '/dashboard/pos' ||
+    (pathname === '/dashboard/product' && !request.cookies.has('access_token'))
+  ) {
     return NextResponse.redirect(new URL('/', request.url));
+  }
 
   return NextResponse.next();
 }
