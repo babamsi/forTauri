5a6c0aa3c326cb0f7a9c53381664171a5588ad3d
bamsi <sabkacade40@gmail.com>
2024-10-23 00:32:41 +0300

    added link

--- store/authApi.js
+++ store/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/api',
+    baseUrl: 'https://pfv4xcnqev.ap-south-1.awsapprunner.com/api',
     prepareHeaders: (headers) => {
       headers.set('Accept', 'application/json');
 
