0fd887d9bdb2379baac6dd4bd8aa2d952b3f6165
bamsi <sabkacade40@gmail.com>
2024-11-19 19:47:16 +0300

    updated

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -400,18 +400,14 @@ export default function TransactionHistory() {
                   <span>Subtotal:</span>
                   <span>
                     $
-                    {
+                    {(
                       // @ts-ignore
+                      selectedTransaction.totalAmount -
                       // @ts-ignore
+                      selectedTransaction.vat +
                       // @ts-ignore
-                      (
-                        selectedTransaction.totalAmount -
-                        // @ts-ignore
-                        selectedTransaction.vat +
-                        // @ts-ignore
-                        selectedTransaction.discount
-                      ).toFixed(2)
-                    }
+                      selectedTransaction.discount
+                    ).toFixed(2)}
                   </span>
                 </div>
                 <div className="flex justify-between">
